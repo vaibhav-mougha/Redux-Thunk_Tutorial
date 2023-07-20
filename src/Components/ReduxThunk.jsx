5333f2c45f01b48a8aadf7bb6cbf194f1d3019fb
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-07-20 18:43:16 +0530

    Added Redux Thunk

---
+++ src/Components/ReduxThunk.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarReduxThunk from "./NavbarReduxThunk";
+
+const ReduxThunk = () => {
+  return (
+    <div>
+      <NavbarReduxThunk />
+    </div>
+  );
+};
+
+export default ReduxThunk;
