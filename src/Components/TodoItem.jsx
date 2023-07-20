9dff85e85fd04a3c13ce573fc0073ada31b6dc3b
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-07-20 18:45:53 +0530

    Added TodoItem

---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, updateTodo } from "../Redux/Todos/todos.action";
+
+const TodoItem = ({ id, value, isCompleted }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div style={{
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center"
+    }}>
+      <h1
+        onClick={() =>
+          dispatch(
+            updateTodo(id, {
+              isCompleted:!isCompleted,
+            })
+          )
+        }
+      >
+      
+         {value} : {isCompleted ? "Completed" : "In Complete"}
+      </h1>
+      <button onClick={()=>dispatch(deleteTodo(id))}>DELETE</button>
+    </div>
+  );
+};
+
+export default TodoItem;
